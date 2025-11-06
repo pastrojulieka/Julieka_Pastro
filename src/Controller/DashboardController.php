d59fe4e4df85cdddab1342c85c15c5fbe6292011
Julieka Pastro <pastrojulieka0@gmail.com>
2025-11-07 00:07:08 +0800

    Add Symfony project files

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        return new Response("Welcome to the dashboard! (Protected Area)");
+    }
+}
